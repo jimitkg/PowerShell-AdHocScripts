25d69930160815d8b2cda309b18d654418f4e9bd
Jimit G <45583144+jimitkg@users.noreply.github.com>
2021-06-16 14:41:44 +1000

    new file

---
+++ dbatools agent job history on a graphical timeine.ps1
@@ -0,0 +1,12 @@
+
+
+Get-DbaAgentJobHistory -SqlInstance sql2016 |
+ConvertTo-DbaTimeline |
+Out-File C:\temp\SQL2016AgentJobHistory.html -Encoding ASCII 
+
+# filter by date
+Get-DbaAgentJobHistory -SqlInstance sql2016 -StartDate '2021-01-15 00:00' -EndDate '2021-01-17 23:59'   |
+ConvertTo-DbaTimeline |
+Out-File C:\temp\SQL2016AgentJobHistory2.html -Encoding ASCII
+
+#testing dev branch commit
