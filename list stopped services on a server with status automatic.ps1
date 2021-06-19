5bb8dccbcd2ae4f99c2db3f561dbdaf8acb19b97
Jim <jimitkg@gmail.com>
2021-06-19 12:31:40 +1000

    init

--- list stopped services on a server with status automatic.ps1
+++ list stopped services on a server with status automatic.ps1
@@ -1,2 +1,5 @@
 Get-Service * | Select-Object DisplayName,Name,StartType,Status|Where-Object {$_.StartType -eq 'Automatic' -and
  $_.Status -ne 'Running'} | Format-Table -Wrap -AutoSize
+
+ #change comment
+ 
