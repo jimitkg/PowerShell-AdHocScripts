d9a2ef3ec18e8f2af767413b794e4363e532f2d6
Jimit G <jimitkg@gmail.com>
2021-07-03 10:05:54 +1000

    enhancements

--- fetch a list of distribution groups from AD.ps1
+++ fetch a list of distribution groups from AD.ps1
@@ -1,4 +1,8 @@
-Get-ADGroup -Filter * | Where-Object {$_.GroupCategory -eq 'Distribution'} | SELECT Name ,DisplayName,mail,Description,ManagedBy
+
+Get-ADGroup -Filter 'GroupCategory -eq "Distribution"' | Select-Object Name ,DisplayName,mail,Description,ManagedBy
+
+
+Get-ADGroup -Filter * | Where-Object {$_.GroupCategory -eq 'Distribution'} | Select-Object Name ,DisplayName,mail,Description,ManagedBy
 
 <#
 Name  
@@ -8,4 +12,8 @@ Description
 ManagedBy
 #>
 
-Get-ADGroup  "@TeamDL" -Properties *| Where-Object {$_.GroupCategory -eq 'Distribution'} | SELECT Name ,DisplayName,mail,Description,ManagedBy
+Get-ADGroup  "@TeamDL" | Select-Object Name ,DisplayName,mail,Description,ManagedBy
+
+
+Get-ADGroup  "@TeamDL" -Properties *| Where-Object {$_.GroupCategory -eq 'Distribution'} | Select-Object Name ,DisplayName,mail,Description,ManagedBy
+
